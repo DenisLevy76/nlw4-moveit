5ec5ae82c9ca4870889426851cce744fb874efd8
Denis Levy <denis.levi11@gmail.com>
2021-02-28 12:37:03 -0300

    Finalizado o frontend página de login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,24 +1,12 @@
-import styles from '../styles/components/Login.module.css';
+import { Login } from "../components/Login";
+import styles from "../styles/components/LoginPage.module.css";
 
-export default function Login(){
+export default function login(){
   return (
     <div className={styles.container}>
-      <div>
-        {/* <img src="icons/biglogo.svg" alt="logo"/> */}
-      </div>
-
-      <div className={styles.mainContainer}>
-        <img src="icons/move-it.svg" alt="logo move.it"/>
-        <strong>Bem-vindo</strong>
-        <div className={styles.title}>
-          <img src="icons/github.svg" alt="github"/>
-          <p>Faça login com seu Github para começar</p>
-        </div>
-        <div className={styles.inputAndButton}>
-          <input type="text" name="username" id="username" placeholder="Digite seu username"/>
-          <button type="button"><img src="icons/seta.svg" alt=""/></button>
-        </div>
-      </div>
+      <div></div>
+      <Login />
+      
     </div>
   )
 }
